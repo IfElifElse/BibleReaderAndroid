f905a4c6d23d49eaa41824a288c287aab342ab39
chaos <haha@yeet.lol>
2019-01-16 18:18:44 -0500

    removing stupid title bar

--- app/src/main/java/com/example/chaos/depspasescouting/MainActivity.java
+++ app/src/main/java/com/example/chaos/depspasescouting/MainActivity.java
@@ -13,11 +13,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //removing header bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //Remove notification bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_main);
         gameScoringIntent  = new Intent(this, GameScoring.class);
     }
